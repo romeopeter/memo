a2af9d98d015d6f9604b79bfa07432249352be92
Romeo <romeomeshack34@gmail.com>
2025-10-08 14:34:36 +0100

    Call function to expose API ro renderer process

---
+++ src/main/main.ts
@@ -0,0 +1,62 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import started from "electron-squirrel-startup";
+import { setupIpcCalls } from "./ipc-calls";
+
+/* -------------------------------------------------------- */
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit();
+}
+
+let mainWindow
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (process.env.MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(process.env.MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `./index.html`));
+  }
+
+  // Open the DevTools.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
+};
+
+// Declare exposed APIs for renderer process
+setupIpcCalls(mainWindow);
+
+/*  ------ App lifecycle ---- */
+
+// This method will be called when Electron has finished initialization and
+// is ready to create browser windows. Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
