852149c9a8bb45259cdf964936ea527eb4862f71
Romeo <romeomeshack34@gmail.com>
2025-11-13 06:26:30 +0100

    Render config

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 /* -------------------------------------------- */
 
 // https://vitejs.dev/config
 export default defineConfig({
+  base: "./"
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
     extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  build: {
+    outDir: "dist/renderer",
   },
 });
