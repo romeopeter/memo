adb4aae5f19dabd746b2d439955e7dbc56a2b344
Romeo <romeomeshack34@gmail.com>
2025-12-08 08:21:39 +0100

    Modified

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/renderer-process/components"),
+      "@lib": path.resolve(__dirname, "./src/renderer-process/lib"),
+      "@utils": path.resolve(__dirname, "./src/renderer-process/lib/utils"),
     },
     extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
   },
